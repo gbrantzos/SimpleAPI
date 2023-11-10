24d80596d005eafc7aada174211cd8335f34aa25
Giorgos Brantzos <g.brantzos@outlook.com>
2023-11-10 09:36:59 +0200

    Use TestContainers for integration testing

--- tests/Infrastructure.IntegrationTests/Setup/DatabaseFixture.cs
+++ tests/Infrastructure.IntegrationTests/Setup/DatabaseFixture.cs
@@ -1,48 +1,74 @@
-using Microsoft.Data.Sqlite;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SimpleAPI.Infrastructure.Persistence;
+using Testcontainers.MySql;
 
 namespace SimpleAPI.Infrastructure.IntegrationTests.Setup;
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class DatabaseFixture : IDisposable
+public class DatabaseFixture : IAsyncLifetime
 {
-    private readonly SqliteConnection? _connection;
-    private readonly IConfigurationRoot _configuration;
+    private readonly IContainer? _container;
+    private SimpleAPIContext? _context;
+    private string? _connectionString;
 
-    public SimpleAPIContext Context { get; }
+    public SimpleAPIContext Context => _context ??
+        throw new InvalidOperationException($"{nameof(DatabaseFixture)} not initialized");
+
+    public string ConnectionString => _connectionString ??
+        throw new InvalidOperationException($"{nameof(DatabaseFixture)} not initialized");
 
     public DatabaseFixture()
     {
         var configurationBuilder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("testsettings.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables(); 
-        _configuration = configurationBuilder.Build();
-        
-        _connection = new SqliteConnection("Filename=:memory:");
-        _connection.Open();
+            .AddEnvironmentVariables();
+        var configuration = configurationBuilder.Build();
 
-        Context = PrepareContext();
+        if (configuration.GetValue<bool>("PrepareContainer"))
+        {
+            _container = new MySqlBuilder()
+                .WithImage(configuration.GetValue<string>("TestContainer:Image"))
+                .WithName($"SimpleAPI_Tests_{Guid.NewGuid():N}")
+                .WithPortBinding(MySqlBuilder.MySqlPort, true)
+                .WithEnvironment("MYSQL_ROOT_PASSWORD", "secret")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(MySqlBuilder.MySqlPort))
+                .Build();
+        }
+        else
+        {
+            _connectionString = configuration.GetConnectionString("SimpleAPI_Tests") ??
+                throw new InvalidOperationException("Connection string for tests is not defined [SimpleAPI_Tests]");
+        }
     }
 
-    private SimpleAPIContext PrepareContext()
+    private void PrepareContext()
     {
-        var connectionString = _configuration.GetConnectionString("SimpleAPI_Tests");
         var options = new DbContextOptionsBuilder<SimpleAPIContext>()
-            //.UseSqlite(_connection)
-            .UseSqlServer(connectionString)
+            .UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString))
             .Options;
-        var context = new SimpleAPIContext(options);
-        context.Database.EnsureCreated();
-
-        return context;
+        _context = new SimpleAPIContext(options);
+        _context.Database.EnsureCreated();
     }
 
-    public void Dispose()
+    public async Task InitializeAsync()
     {
-        Context.Database.EnsureDeleted();
-        _connection?.Dispose();
+        if (_container is not null)
+        {
+            await _container.StartAsync();
+            var port = _container.GetMappedPublicPort(MySqlBuilder.MySqlPort);
+            _connectionString = $"server=localhost;user=root;password=secret;database=SimpleAPI;port={port}";
+        }
+        PrepareContext();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _context!.Database.EnsureDeletedAsync();
+        if (_container is not null)
+            await _container.StopAsync();
     }
 }
